881cc7bd799ca83c222a1ad46152c49761f64f4f
MariaJRPessoa <MariaJRPessoa@users.noreply.github.com>
2023-09-16 22:06:02 +0200

    Implemented the unit converter

--- src/app.js
+++ src/app.js
@@ -25,24 +25,29 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.condition.description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute(
     "src",
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
+  celsiusTemperature = response.data.temperature.current;
   iconElement.setAttribute("alt", response.data.condition.description);
 }
+
 function search(city) {
   let apiKey = "b34df24b3a92f0da2bte873o40353eff";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -54,6 +59,32 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+
+function displayFTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  cLink.classList.remove("active");
+  fLink.classList.add("active");
+  let fTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fTemperature);
+}
+
+function displayCTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  fLink.classList.remove("active");
+  cLink.classList.add("active");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fLink = document.querySelector("#fahrenheit-link");
+fLink.addEventListener("click", displayFTemperature);
+
+let cLink = document.querySelector("#celsius-link");
+cLink.addEventListener("click", displayCTemperature);
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
